f122beb25d5927899f21e5f76d6b37e5b0a94f69
FarosaniaAmanda <farosaniaamanda60@gmail.com>
2026-09-16 15:05:09 +0700

    oca

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MonitoringController;
+use App\Http\Controllers\LaporanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Monitoring
+Route::get('/admin/monitoring', [MonitoringController::class, 'index'])
+    ->name('admin.monitoring');
+
+Route::get('/admin/monitoring/{id}', [MonitoringController::class, 'detail'])
+    ->name('admin.monitoring.detail');
+
+// Laporan
+Route::get('/admin/laporan', [LaporanController::class, 'index'])
+    ->name('admin.laporan');
